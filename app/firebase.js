44d964bfca3f2dff00f317dd0686d7d7300fbbed
eunjaelim <imeunjae361@gmail.com>
2023-10-24 11:48:27 +0900

    vercel

---
+++ app/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyANM0mZDsXsGpqh2mAZAGOar5FoGq1MOz8",
+  authDomain: "instagram-215cf.firebaseapp.com",
+  projectId: "instagram-215cf",
+  storageBucket: "instagram-215cf.appspot.com",
+  messagingSenderId: "6065144313",
+  appId: "1:6065144313:web:122289344503c861f33d28"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
